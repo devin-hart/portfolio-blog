917a678a417b20aa83421b794b7edb715e4281f5
devin hart <devohart@gmail.com>
2025-03-07 13:25:40 -0500

    change other title

--- app/other/page.tsx
+++ app/other/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section className="hs-screeen">
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Work</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Other</h1>
       <BlogPosts />
     </section>
   )
