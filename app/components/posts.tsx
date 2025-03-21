6c9e32e4e1cb0d5845bd5b9f7acdc26874bc8d09
devin hart <devohart@gmail.com>
2025-03-21 16:52:35 -0400

    add mtg life tracker post

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -23,11 +23,11 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-1"
             href={`/blog/${post.slug}`}
           >
             <div className="w-full grid md:grid-cols-[125px_1fr] md:gap-x-4 md:gap-y-4">
-              <p className="w-[150px] tabular-nums">
+              <p className="w-[150px] tabular-nums text-gray-400">
                 {post.metadata.publishedAt}
               </p>
               <p className="tracking-tight">
