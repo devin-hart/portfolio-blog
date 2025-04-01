22a9b201eeece8641c96e4929633506e522a2eb2
devin hart <devohart@gmail.com>
2025-04-01 15:04:04 -0400

    easter egg edit

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -28,8 +28,8 @@ thanks for your checking out my portfolio
 the site was made using react and typescript
 it was deployed on vercel
 consider hiring me
-<3 devin hart
-      `);
+devin %c<3
+      `, "color: red; font-weight: bold;");
       }, []);
   return (
     <footer className="pb-8 mt-16">
