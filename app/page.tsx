d56affd75be8be9f1d3cae1df046dc82795f9aa9
devin hart <devohart@gmail.com>
2025-10-23 12:34:29 -0400

    add scroll to main element, add defiantsystem blog, edit homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,9 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Devin Hart | Web Dev
       </h1>
-      <p >
-        {`There is code, and there is the absence of code. I have spent my time in the former, shaping it, bending it to purpose. HTML, CSS, JavaScript. The bones, the muscle, the blood. React, GraphQL, RESTful APIs. Tools of the trade. The means by which a thing becomes more than a thing.`}</p>
-
-        <p >{`I have built for commerce. Watched the ebb and flow of traffic, the invisible hands clicking, scrolling, buying. The numbers rise and fall like tides. I have seen what makes a page fast and what makes it slow. What draws the eye and what sends it away. Accessibility, SEO, performance. Words that are more than words.`}</p>
-
-        <p >{`And then there’s the unseen work. The tools behind the tools. Quiet things built to shape louder ones. A script here, a library there—nothing glamorous, but everything smoother because of them. The kind of work no one notices unless it’s missing. The kind that holds the rest together.`}</p>
-
-        <p >{`I build. I refine. I deploy. There is always more to do. Always another problem to solve. I step forward into the unknown. And I build again.`}
-      </p>
+      <p >Ideas turned into form, logic into movement.</p>
+      <br />
+      <img src="/images/d2.webp" alt="me" />
     </section>
   )
 }
